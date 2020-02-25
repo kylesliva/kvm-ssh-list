5565119f572e606cfa3c21440877b5a1620e468e
Kyle Sliva <kyle@example.com>
2020-02-24 21:15:56 -0600

    add DNS query support

--- printGuests.py
+++ printGuests.py
@@ -2,16 +2,21 @@
 import re
 import subprocess
 import logging
+import socket
 
 '''
 #todo
 * add argparse 
+    * for setting log level
 * opt selector 
+* exception handling
+* more verbose logging
 '''
 
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s- %(message)s')
 
 def main():
+    # pull out arp cache from shell. is there a better way to use socket for this?
     arpCache = str(subprocess.check_output(["arp", "-a"]))
     logging.debug(arpCache)
     logging.debug(type(arpCache))
@@ -19,7 +24,7 @@ def main():
     
     opts = re.findall("192\.168\.122\.\d{1,3}", arpCache) 
     
-    printOptions(opts)
+    connectHost(printOptions(opts))
 
 #checks for KVM IP
 def match(ip):
@@ -33,8 +38,29 @@ def printOptions(opts):
     selectionIndex = -1 
     for num, item in enumerate(opts):
         logging.debug(num)
-        print(f"{num}) {item} ")
-    selectionIndex = int(input("Please select an option from the above list"))
+        print(f"{num}) {socket.gethostbyaddr(item)[0]} ")
+    selectionIndex = int(input("Please select an option from the above list: "))
+    logging.debug(f"selection index: {selectionIndex}")
+    
+    try:
+        output = opts[selectionIndex]
+    except Exception as e:
+        print("please enter a valid list index")
+    
+    logOutput = (
+                f"ip: {output}\n"
+                f"hostname: {socket.gethostbyaddr(output)}"
+    )
+    
+    logging.debug(
+                f"ip: {output}\n"
+                f"hostname: {socket.gethostbyaddr(output)[0]}"
+    )
+    return output
+
+#initiates SSH connection to ip
+def connectHost(ip):
+    subprocess.run(["ssh", ip])
         
 
 
